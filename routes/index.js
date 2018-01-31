732e135f9b00ee79623d1063b8b328a829cd96e7
einsteinder <einsteinder@gmail.com>
2018-01-30 22:26:54 -0500

    add image path

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var traverse = require('fs-tree-traverse');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  var imageList = traverse.list(path.join(__dirname, '../public/images'), function (err, files) {
+    res.render('index', { title: 'Express',imageList:files});
+  });
+
+ 
 });
 
 module.exports = router;
